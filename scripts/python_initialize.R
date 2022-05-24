8ad33534b7b6f94691495cb6c38fd5e01181925d
ging3RR <strangerglint6@gmail.com>
2022-05-24 16:54:12 +0200

    needed to run the right version of python

---
+++ scripts/python_initialize.R
@@ -0,0 +1,12 @@
+Kwic
+
+head(kwic(my_corpus_token, pattern = "uhr"))
+
+
+#load this before runing python code
+
+#make sure the code can run
+library("reticulate")
+path_to_python <- "C:/Users/lukas/anaconda3/python.exe"
+use_python(path_to_python)
+repl_python()
