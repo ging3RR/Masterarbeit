31c73899a47c176767a4c595df712595250e44c4
ging3RR <strangerglint6@gmail.com>
2022-06-08 15:11:34 +0200

    Added frequency analysis

--- scripts/Vergleich.R
+++ scripts/Vergleich.R
@@ -1,5 +1,6 @@
 
 source(file = "E:/R Projects/Daten_MA/Masterarbeit/scripts/packages.R")
+source(file = "E:/R Projects/Daten_MA/Masterarbeit/scripts/stopwords.R")
 tic("Vergleich")
 load(file = "E:/R Projects/Daten_MA/Masterarbeit/data/TextArbeitszeit.rda")
 
@@ -18,32 +19,43 @@ Arbeitgeber_csv$complete_text <- gsub(pattern = "\\..\\s+",
 Arbeitgeber_csv$complete_text <- gsub(pattern = "\\. .\\s+", 
                                       replacement = "", 
                                       x = Arbeitgeber_csv$complete_text) 
-#create variable to seperate the corpora
 
+#create variable to seperate the corpora
 Arbeitgeber_csv$organisation <- "Arbeitgeber"
 TextArbeitszeit$organisation <- "Arbeitnehmer"
 
 #create corpus objects
-
 corpus_Arbeitgeber <- corpus(Arbeitgeber_csv, text_field = "complete_text")
 corpus_Arbeitnehmer <- corpus(TextArbeitszeit)
 
 corpus_combined <- corpus_Arbeitgeber + corpus_Arbeitnehmer
 
+
 #make a dfm
-
-#stopwords
-sw<- c(tm::stopwords("german"), "dass")
-
 corpus_dfm <- tokens(corpus_combined, remove_punct = T, 
                      remove_symbols = T,
                      remove_numbers = T) %>% 
   tokens_remove(sw) %>% tokens_group(groups = organisation) %>% 
   dfm()
 
-#calculate keyness
-result_keyness <- textstat_keyness(corpus_dfm, target = "Arbeitnehmer")
+#calculate keyness (https://quanteda.io/articles/pkgdown/examples/plotting.html)
+result_keyness <- textstat_keyness(corpus_dfm, target = "Arbeitgeber")
 
 #plot
 textplot_keyness(result_keyness)
+
+
+#frequency (https://quanteda.io/articles/pkgdown/examples/plotting.html)
+corpus_dfm_weight <- dfm_weight(x = corpus_dfm ,scheme = "prop")
+
+freq_weight <- textstat_frequency(corpus_dfm_weight, n = 15,
+                                  groups = corpus_dfm_weight@docvars$organisation)
+
+ggplot(data = freq_weight, aes(x = nrow(freq_weight):1, y = frequency)) +
+  geom_point() +
+  facet_wrap(~ group, scales = "free") +
+  coord_flip() +
+  scale_x_continuous(breaks = nrow(freq_weight):1,
+                     labels = freq_weight$feature) +
+  labs(x = NULL, y = "Relative frequency")
 toc()
