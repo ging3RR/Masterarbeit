56aec5fbeabe7223779e2d760cfb9f42572059f2
ging3RR <strangerglint6@gmail.com>
2022-06-08 15:11:12 +0200

    Create stopwords.R

---
+++ scripts/stopwords.R
@@ -0,0 +1,15 @@
+#All the stopwords
+
+sw<- c(tm::stopwords("german"), #standard stopwords
+       "dass", #was not included in standard stopwords
+       "max", # Max Weber is always cited in the beginning of the journal
+       "weber",
+       "robert",#Robert Bratschi as well
+       "bratschi",
+       "de", #word from Arbeitgeber Korpus probably from french
+       tm::stopwords("french"), #remove all french stopwords
+       "heft", #header information
+       "monbijoustrasse",#header information
+       "no", #number, header information
+       "schweizerische", #header information
+       "arbeitgeber-zeitung") #header information
