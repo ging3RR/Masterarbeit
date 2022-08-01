2ccf6937a4fcecb0a8ef0ce9a192c0313ef635c0
ging3RR <strangerglint6@gmail.com>
2022-08-01 16:24:30 +0200

    Create validity.R

---
+++ scripts/validity.R
@@ -0,0 +1,25 @@
+Arbeitgeber_csv <- Arbeitgeber_csv %>%  select(doc_id, complete_text,date)
+corpus <- corpus(Arbeitgeber_csv,  text_field = "complete_text")
+tokenized <- tokens(corpus)
+kwic <- kwic(tokenized, pattern = "meister")
+kwic
+
+Streik <- read.csv("E:/R Projects/Daten_MA//Masterarbeit/data/Streik_1922_1971.csv")
+
+
+## Validität
+
+Hans_Arbeitnehmer1 <- intruderWords(beta = Prototype_LDA$topics, numIntruder = 0:1, numTopwords = 10)
+
+Hans_Arbeitnehmer2 <- intruderWords(beta = Prototype_LDA$topics, numIntruder = 0:1, numTopwords = 10)
+
+Hans_Arbeitnehmer3 <- intruderWords(beta = Prototype_LDA$topics, numIntruder = 0:1, numTopwords = 10)
+
+
+Hans_Arbeitgeber1 <- intruderWords(beta = Prototype_LDA_Arbeitgeber$topics, numIntruder = 0:1, numTopwords = 10)
+
+Hans_Arbeitgeber2 <- intruderWords(beta = Prototype_LDA_Arbeitgeber$topics, numIntruder = 0:1, numTopwords = 10)
+
+Hans_Arbeitgeber3 <- intruderWords(beta = Prototype_LDA_Arbeitgeber$topics, numIntruder = 0:1, numTopwords = 10)
+
+
